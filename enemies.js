acfc866aab0974a778a8255bc2bd0b7cffa4c939
Trto0191 <travinatorres@gmail.com>
2022-05-25 15:15:58 -0700

    Upload

--- enemies.js
+++ enemies.js
@@ -4,7 +4,7 @@ class Enemy {
     this.r = 50;
     this.x = random(w);
     this.y = 0 - this.r;
-    this.speed = 5;
+    this.speed = 15;
   }
 
   display(){
--- sketch.js
+++ sketch.js
@@ -1,15 +1,11 @@
 'use strict';
 
-var start;
-var begin;
-var nextlvl2;
-var nextlvl3;
-var restart;
+// var begin;
 
 let state ='title';
 let cnv;
 let points = 0;
-let lives = 2;
+let lives = 3;
 let w = 600;
 let h = 600;
 let player = 1;
@@ -18,9 +14,7 @@ let enemies = [];
 let playerImg;
 let coinImg;
 let enemyImg;
-let darkImg;
 let bckgImg;
-let dayImg;
 let titleImg;
 
 
@@ -29,20 +23,17 @@ function preload(){
   playerImg = loadImage('assets/butterfly.png');
   coinImg = loadImage('assets/light1.png');
   enemyImg = loadImage('assets/rain1.png');
-
 }
 
 function setup(){
- darkImg = loadImage('assets/dark_bkgd.png')
  bckgImg = loadImage('assets/level_bckg.png');
- dayImg = loadImage('assets/daylight_bkgd.png');
  titleImg = loadImage('assets/background.png');
 
  cnv = createCanvas(w, h);
   //frameRate(200);
 
   imageMode(CENTER);
-  // rectMode(CENTER);
+  rectMode(CENTER);
   textFont('monospace');
 
   player = new Player();
@@ -51,107 +42,23 @@ function setup(){
   coins.push(new Coin());
   enemies.push(new Enemy());
 
-  // clickable "start" button - ONLY TEXT from title to tutorial
-  start = new Clickable();
-    start.locate(w/2.130, h/1.5);
-    start.color = '#dddddd00';
-    start.text = "";
-    start.strokeWeight = 0;
-    start.image = playerImg;
-    start.fitImage = true;
-    start.width = 37;
-    start.height = 30;
-    start.onHover = function() {
-      start.imageScale = 2.7;
-      start.image = playerImg;
-    }
-    start.onPress = function() {
-      state = 'guide';
-    }
-    start.onOutside = function() {
-      start.imageScale = 2.5;
-      start.image = playerImg;
-}
-begin = new Clickable();
-  begin.locate(w/2.130, h/1.2);
-  begin.color = '#dddddd00';
-  begin.text = "";
-  begin.strokeWeight = 0;
-  begin.image = playerImg;
-  begin.fitImage = true;
-  begin.width = 37;
-  begin.height = 30;
-  begin.onHover = function() {
-    begin.imageScale = 2.7;
-    begin.image = playerImg;
-  }
-  begin.onPress = function() {
-    state = 'level 1';
-  }
-  begin.onOutside = function() {
-    begin.imageScale = 2.5;
-    begin.image = playerImg;
-  }
-  nextlvl2 = new Clickable();
-    nextlvl2.locate(w/2.130, h/1.5);
-    nextlvl2.color = '#dddddd00';
-    nextlvl2.text = "";
-    nextlvl2.strokeWeight = 0;
-    nextlvl2.image = playerImg;
-    nextlvl2.fitImage = true;
-    nextlvl2.width = 37;
-    nextlvl2.height = 30;
-    nextlvl2.onHover = function() {
-      nextlvl2.imageScale = 2.7;
-      nextlvl2.image = playerImg;
-    }
-    nextlvl2.onPress = function() {
-      state = 'level 2';
-    }
-    nextlvl2.onOutside = function() {
-      nextlvl2.imageScale = 2.5;
-      nextlvl2.image = playerImg;
-  }
-  nextlvl3 = new Clickable();
-    nextlvl3.locate(w/2.130, h/1.5);
-    nextlvl3.color = '#dddddd00';
-    nextlvl3.text = "";
-    nextlvl3.strokeWeight = 0;
-    nextlvl3.image = playerImg;
-    nextlvl3.fitImage = true;
-    nextlvl3.width = 37;
-    nextlvl3.height = 30;
-    nextlvl3.onHover = function() {
-      nextlvl3.imageScale = 2.7;
-      nextlvl3.image = playerImg;
-    }
-    nextlvl3.onPress = function() {
-      state = 'level 3';
-    }
-    nextlvl3.onOutside = function() {
-      nextlvl3.imageScale = 2.5;
-      nextlvl3.image = playerImg;
-}
-  restart = new Clickable();
-    restart.locate(w/2.130, h/1.5);
-    restart.color = '#dddddd00';
-    restart.text = "";
-    restart.strokeWeight = 0;
-    restart.image = playerImg;
-    restart.fitImage = true;
-    restart.width = 37;
-    restart.height = 30;
-    restart.onHover = function() {
-      restart.imageScale = 2.7;
-      restart.image = playerImg;
-    }
-    restart.onPress = function() {
-      state = 'title';
-    }
-    restart.onOutside = function() {
-      restart.imageScale = 2.5;
-      restart.image = playerImg;
-    }
+//   // clickable "begin" button - ONLY TEXT TOGETHER
+//   begin = new Clickable();
+//   begin.locate(200, 360);
+//   begin.resize(100, 50);
+//   begin.text = "begin";
+//   begin.onHover = function () {
+//   this.color = "#FFDE00";
+//   this.noTint = false;
+//   this.tint = "#FF0000";
+// }
+// begin.onOutside = function () {
+//   this.color = "#FFFFFF";
+//   this.noTint = true;
+// }
+// begin.onPress = function () {
+//   state = 'tutorial';
+// }
 }
 
 function draw() {
@@ -159,33 +66,23 @@ function draw() {
   switch (state){
     case 'title':
       title();
-      // cnv.mouseClicked(titleMouseClicked);
+    cnv.mouseClicked(titleMouseClicked);
       break;
-    case 'guide':
-      tutorialStage();
+    case 'tutorial':
+      tutorial();
+    cnv.mouseClicked(tutorialMouseClicked);
       break;
     case 'level 1':
       level1();
-      break;
-    case 'level 2':
-      level2();
-      break;
-    case 'level 3':
-      level3();
+      cnv.mouseClicked(level1MouseClicked);
       break;
     case 'you win':
       youWin();
-      break;
-    case 'you win 2':
-      youWin2();
-      break;
-    case 'you win 3':
-      youWin3();
-      cnv.mouseClicked(youWin3MouseClicked);
+      cnv.mouseClicked(youWinMouseClicked);
       break;
     case 'game over':
       gameOver();
-    cnv.mouseClicked(gameOverMouseClicked);
+      cnv.mouseClicked(gameOverMouseClicked);
       break;
     default:
       break;
@@ -193,6 +90,15 @@ function draw() {
 }
 
 function keyPressed() {
+// Press "s" to change state - change state not functional
+//   if (state === 'title' || state === 'you win') {
+//   if (key === 's' || key === 'S') {
+//   state = 'level l';
+// }
+// } else if (state === 'level 1') {
+//   if (key === 's' || key === 'S' ) {
+//
+// }
 
   if (keyCode == LEFT_ARROW){
     player.direction = 'left'
@@ -235,13 +141,13 @@ function keyReleased(){
 }
 
 function title(){
-  background(0);
   imageMode(CENTER);
   image(titleImg, w/2, h/2, 600, 600);
-  // textAlign(LEFT);
-  start.draw();
 
-  // image(playerImg, w/2, h/1.8)
+  // keyPressed.draw();
+
+
+  image(playerImg, w/2, h/1.8)
   image(coinImg, w/2, h/3.2, 200, 200);
 
   strokeWeight(3);
@@ -254,24 +160,19 @@ function title(){
   noStroke();
   textSize(30);
   fill(255);
-  text("Start", width / 2, height / 1.25);
+  text('click anywhere to start', w/2, h/1.4);
 
 }
 
 function titleMouseClicked(){
-  // console.log('canvas is clicked on title page');
-  // state = 'guide'
+  console.log('canvas is clicked on title page');
+  state = 'tutorial'
 }
 
-function tutorialStage(){
-  background(0);
-  image(titleImg, w/2, h/2, 600, 600);
-
+function tutorial(){
+  image(titleImg,  w/2, h/2, 600, 600);
   fill(255);
-  push();
-  rectMode(CENTER);
   rect(width * .5, height * .345, 345, 135, 10);
-  pop();
 
 image(coinImg, 250, 165);
 image(enemyImg, 250, 206);
@@ -287,10 +188,10 @@ text(" Press ← → ↑ ↓ to move the butterfly", width/2, height * 0.42);
 
 fill(255);
 textSize(20);
-textAlign(CENTER);
-text("Help! Safely guide the butterfly through the storm. Its a cold and rainy day so try to avoid as much RAIN and collect the LIGHT to keep the butterfly warm. Safe travels!", width / 7.5, height / 1.8, 450);
+text("Help! Safely guide the butterfly through the storm. Its a cold and rainy day so try to avoid as much RAIN and collect the LIGHT to keep the butterfly warm. Safe travels!", width / 2, height / 1.8, 450);
 fill(255);
-text("Start", width / 2, height / 1.05);
+textSize(20);
+text("Click anywhere to start", width / 2, height / 1.18);
 
 
 strokeWeight(3);
@@ -300,22 +201,20 @@ textSize(40);
 textAlign(CENTER);
 text("Tutorial", width/2, height * 0.12);
 
-begin.draw();
+image(playerImg, w/2, h/1.11);
+
 }
 
 function tutorialMouseClicked(){
-  // console.log('canvas is clicked on title page');
+  console.log('canvas is clicked on title page');
   state = 'level 1'
 }
 
 function level1(){
-  background(0);
-
-  push();
   imageMode(CENTER);
-  imageMode(CENTER);
-  image(darkImg, w/2, h/2, 600, 600);
-  pop();
+  image(bckgImg, w/2, h/2, 600, 600);
+  // imageMode(CENTER);
+  // image(bckgImg, w/2, h/2, 600, 600);
 
   if (random(1) <= 0.01){
     coins.push(new Coin());
@@ -373,9 +272,7 @@ function level1(){
   strokeWeight(3)
   stroke(255);
   fill(0);
-  textSize(30);
   text(`score: ${points} / 1000`, w/4, h - 30);
-  text('Level 1', w/2, h/8.5);
 
 // check point values to win or lose the game
   if (points >= 1000){
@@ -383,174 +280,10 @@ function level1(){
   } else if (points <= -1){
     state = 'game over';
   }
+
 }
 
 function level1MouseClicked(){
-  points++;
-  console.log('points = + points');
-
-  if (points >= 10){
-    state = 'level 2';
-  }
-}
-
-function level2(){
-  background(0);
-  imageMode(CENTER);
-  image(bckgImg, w/2, h/2, 600, 600);
-
-  if (random(1) <= 0.01){
-    coins.push(new Coin());
-  }
-
-  if (random(1) <= 0.03){
-    enemies.push(new Enemy());
-  }
-
-  player.display();
-  player.move();
-
-  //iterating through coins array to display and move them
-  // using for loop
-  for (let i = 0; i < coins.length; i++){
-    coins[i].display();
-    coins[i].move();
-    }
-
-  //iterating through enemies array to display and move them
-  // using for loop
-  for (let i = 0; i < enemies.length; i++){
-    enemies[i].display();
-    enemies[i].move();
-    }
-
-
-  // check for collision with coins, if there is a colliion increase points by 1 AND splice that coin out of array
-  // need to iterate backyards through array
-  for (let i = coins.length - 1; i >= 0; i--){
-    // check for collision with player
-    if (dist(player.x, player.y, coins[i].x, coins[i].y) <= (player.r + coins[i].r) / 2){
-      points += 100;
-      console.log(points);
-      coins.splice(i, 1);
-    } else if (coins[i].y > h){
-      coins.splice(i, 1);
-      console.log('coin is out of town');
-    }
-  }
-
-  // check for collision with enemies, if there is a colliion increase points by 1 AND splice that enemy out of array
-  // need to iterate backyards through array
-  for (let i = enemies.length - 1; i >= 0; i--){
-    // check for collision with player
-    if (dist(player.x, player.y, enemies[i].x , enemies[i].y) <= (player.r + enemies[i].r) / 2.4){
-      points -= 100;
-      console.log(points);
-      enemies.splice(i, 1);
-    } else if (enemies[i].y > h){
-      enemies.splice(i, 1);
-      console.log('enemy is out of town');
-    }
-  }
-  strokeWeight(3)
-  stroke(255);
-  fill(0);
-  text(`score: ${points} / 2000`, w/4, h - 30);
-  text('Level 2', w/2, h/8.5);
-
-
-// check point values to win or lose the game
-  if (points >= 2000){
-    state = 'you win 2';
-  } else if (points <= -1){
-    state = 'game over';
-  }
-}
-
-function level2MouseClicked(){
-//   points++;
-//   console.log('points = + points');
-
-  if (points >= 10){
-    state = 'level 3';
-  }
-}
-
-function level3(){
-  background(0);
-  imageMode(CENTER);
-  image(dayImg, w/2, h/2, 600, 600);
-
-    if (random(1) <= 0.01){
-      coins.push(new Coin());
-    }
-
-    if (random(1) <= 0.03){
-      enemies.push(new Enemy());
-    }
-
-    player.display();
-    player.move();
-
-    //iterating through coins array to display and move them
-    // using for loop
-    for (let i = 0; i < coins.length; i++){
-      coins[i].display();
-      coins[i].move();
-      }
-
-    //iterating through enemies array to display and move them
-    // using for loop
-    for (let i = 0; i < enemies.length; i++){
-      enemies[i].display();
-      enemies[i].move();
-      }
-
-
-    // check for collision with coins, if there is a colliion increase points by 1 AND splice that coin out of array
-    // need to iterate backyards through array
-    for (let i = coins.length - 1; i >= 0; i--){
-      // check for collision with player
-      if (dist(player.x, player.y, coins[i].x, coins[i].y) <= (player.r + coins[i].r) / 2){
-        points += 100;
-        // console.log(points);
-        coins.splice(i, 1);
-      } else if (coins[i].y > h){
-        coins.splice(i, 1);
-        console.log('coin is out of town');
-      }
-    }
-
-    // check for collision with enemies, if there is a colliion increase points by 1 AND splice that enemy out of array
-    // need to iterate backyards through array
-    for (let i = enemies.length - 1; i >= 0; i--){
-      // check for collision with player
-      if (dist(player.x, player.y, enemies[i].x , enemies[i].y) <= (player.r + enemies[i].r) / 2.4){
-        points -= 100;
-        console.log(points);
-        enemies.splice(i, 1);
-      } else if (enemies[i].y > h){
-        enemies.splice(i, 1);
-        console.log('enemy is out of town');
-      }
-    }
-    strokeWeight(3)
-    stroke(255);
-    fill(0);
-    text(`score: ${points} / 3000`, w/4, h - 30);
-    text('Level 3', w/2, h/8.5);
-
-
-  // check point values to win or lose the game
-    if (points >= 3000){
-      state = 'you win 3';
-    } else if (points <= -1){
-      state = 'game over';
-    }
-
-}
-
-function level3MouseClicked(){
 //   points++;
 //   console.log('points = + points');
 
@@ -560,7 +293,6 @@ function level3MouseClicked(){
 }
 
 function youWin(){
-  background(0);
   imageMode(CENTER);
   image(titleImg, w/2, h/2, 600, 600);
 
@@ -573,78 +305,17 @@ function youWin(){
   noStroke();
   textSize(30);
   fill(255);
-  text('Next Level', w/2, h / 1.6);
+  text('click anywhere to restart', w/2, h / 1.5);
 
   image(coinImg, w/2, h/2.8, 45, 50);
-  nextlvl2.draw();
-
 }
 
 function youWinMouseClicked(){
-  console.log('canvas is clicked on you win page');
-
-  state = 'level 2';
-  points = 0;
-}
-function youWin2(){
-  background(0);
-  imageMode(CENTER);
-  image(titleImg, w/2, h/2, 600, 600);
-
-  strokeWeight(3);
-  stroke(255);
-  fill(0);
-  textSize(80);
-  text('YOU WIN', w/2, h/2);
-
-  noStroke();
-  textSize(30);
-  fill(255);
-  text('Next Level', w/2, h / 1.6);
-
-  image(coinImg, w/2, h/2.8, 45, 50);
-
-  nextlvl3.draw();
-}
-
-function youWin2MouseClicked(){
-
-  state = 'level 3';
-   points = 0;
-}
-function youWin3(){
-  background(0);
-  imageMode(CENTER);
-  image(titleImg, w/2, h/2, 600, 600);
-
-  strokeWeight(3);
-  stroke(255);
-  fill(0);
-  textSize(80);
-  text('YOU WIN', w/2, h/2);
-
-  noStroke();
-  textSize(30);
-  fill(255);
-  text('Title Screen', w/2, h / 1.6);
-
-  image(coinImg, w/2, h/2.8, 45, 50);
-
-  restart.draw();
-}
-
-function youWin3MouseClicked(){
-  // console.log('canvas is clicked on you win 3 page');
-
-  if (state = 'guide') {
-  } else {
-  state = 'level 1';
-}
-   points = 0;
+  state = 'title';
+  points = 1;
 }
 
 function gameOver(){
-  background(0);
   imageMode(CENTER);
   image(titleImg, w/2, h/2, 600, 600);
 
@@ -667,7 +338,6 @@ function gameOver(){
     fill(255);
     text('click anywhere to play again', w/2, h / 1.5);
 
-
   } else {
     // game over
     strokeWeight(3);
@@ -679,22 +349,16 @@ function gameOver(){
     fill(255);
     textSize(30);
     text('click anywhere to restart', w/2, h / 1.5);
-
-    restart.draw();
   }
 }
 
 function gameOverMouseClicked(){
-  console.log('canvas is clicked on game over page');
-  // state = 'title'
-
     if (lives >= 0) { // this means they have 0 lives going into it b/c life already taken away in gameOver() function
       lives--; // if you have a life, you lose one!
       state = 'level 1';
     } else {
-      state = 'level 2';
+      state = 'title';
     }
 
   points = 0;
-
 }
